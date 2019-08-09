1d03becd4af83171fcedb2f269852414fabfe715
Tammi Lee <tammilee219@gmail.com>
2019-08-08 19:49:45 -0700

    5 different time displays

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace timeDisplay{
+    public class HomeController : Controller{
+        [HttpGet("")]
+        public ViewResult Index(){
+            return View();
+        }
+    }
+}
